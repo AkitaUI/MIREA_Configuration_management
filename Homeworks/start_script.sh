8c0b69aa233b3ed811bdd6f02b583109a5f6050f
AkitaUI <vendiedonn@gmail.com>
2024-11-14 17:24:46 +0300

    Update start_script.sh

--- Homeworks/start_script.sh
+++ Homeworks/start_script.sh
@@ -1,11 +1,4 @@
-echo "Подготовка к сборке..."
-mkdir -p virtual_fs
-
-echo "Запуск тестов..."
-py -m unittest discover -s . -p "Test_*.py" > test_results.txt
-cat test_results.txt
-
-echo "Очистка временных файлов..."
-rm -rf __pycache__ build dist virtual_fs
-
-echo "Скрипт завершен."
+ls
+uptime
+uname
+exit
